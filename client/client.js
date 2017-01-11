4e68f690970619709d32f24f3864fd7f4f887f60
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2017-01-11 19:49:51 +0000

    commit

--- client/client.js
+++ client/client.js
@@ -9,10 +9,21 @@ myApp.controller('mainController', function($scope, $http, $window) {
                 $scope.userName = response.data;
                 console.log(response.data + ' Get My Username ' + $scope.userName);
                 
+                $scope.string_API = "/getMyPins/" +  $scope.user_name;  
+                console.log("Requesting all my Pins:  " + $scope.string_API);
+                $http.get($scope.string_API)
+                    .then(function(response) {
+                console.log("Response received: "+ response.data); 
+                if(response.data != null){
+                    $scope.myPins=response.data.user_pins;
+                    console.log(" My Pins List: " + JSON.stringify(response.data.user_pins));
+                }
+                 });    
+                
             }
         });
-
-
+        
+   
     $scope.login = function(userName, userPwd) {
         $http.get("/login/" + userName)
             .then(function(response) {
@@ -50,8 +61,18 @@ myApp.controller('mainController', function($scope, $http, $window) {
             });
     };
     
+    $scope.removeImage = function (userName,imgUrl){
+        $scope.user_name= userName;
+        $scope.string_API = "/img/" +  $scope.user_name + "/remove/" + encodeURIComponent(imgUrl);  
+        console.log("Request to remove image: " + $scope.string_API);
+        $http.get($scope.string_API)
+            .then(function(response) {
+                console.log("Your Image: " + encodeURIComponent(imgUrl) + " was removed.");
+            });
+    };
+    
     $scope.getMyPins = function (userName){
-        $scope.user_name="u2" ; //userName;
+        $scope.user_name=userName;
         $scope.string_API = "/getMyPins/" +  $scope.user_name;  
         console.log("Requesting all my Pins:  " + $scope.string_API);
         $http.get($scope.string_API)
